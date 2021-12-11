e619a13c8f304e41cb8f6a44e0c05d5705ebaeff
haiya9 <95959074+haiya9@users.noreply.github.com>
2021-12-11 18:39:38 +0800

    在线修改

--- HelloGit/src/main/java/com/haiya/git/HelloGit.java
+++ HelloGit/src/main/java/com/haiya/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("版本测试！");
         System.out.println("创建分支dev");
+        System.out.println("在线修改");
     }
 }
