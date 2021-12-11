6f3bf184c08d160d778ddea26e3873780926bbab
haiya9 <jiajiaolei@163.com>
2021-12-11 17:56:16 +0800

    Git版本测试

--- HelloGit/src/main/java/com/haiya/git/HelloGit.java
+++ HelloGit/src/main/java/com/haiya/git/HelloGit.java
@@ -4,6 +4,5 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("版本测试！");
-        System.out.println("版本测试2");
     }
 }
